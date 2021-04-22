acf242b5f4c2071d9562f1bfe68df12e9e58e60b
ggrachdev <ggrachdev@yandex.ru>
2021-04-22 15:39:06 +0400

    Change README.md

--- ggrachdev.fish_generator/include.php
+++ ggrachdev.fish_generator/include.php
@@ -1,6 +1,8 @@
 <?
 
-include_once 'classes/general/Libs/faker/src/autoload.php';
+if (!class_exists('\Faker\Factory')) {
+    include_once 'classes/general/Libs/faker/src/autoload.php';
+}
 
 \Bitrix\Main\Loader::registerAutoLoadClasses('ggrachdev.fish_generator', [
     // exceptions
@@ -9,7 +11,6 @@ include_once 'classes/general/Libs/faker/src/autoload.php';
     "\GGrach\FishGenerator\Exceptions\GeneratePhotoException" => "classes/general/FishGenerator/Exceptions/GeneratePhotoException.php",
     "\GGrach\FishGenerator\Exceptions\GeneratorTypeException" => "classes/general/FishGenerator/Exceptions/GeneratorTypeException.php",
     "\GGrach\FishGenerator\Exceptions\SearchIblockException" => "classes/general/FishGenerator/Exceptions/SearchIblockException.php",
-    
     // other
     "\GGrach\FishGenerator\Debug" => "classes/general/FishGenerator/Debug.php",
     "\GGrach\FishGenerator\Generators\PhotoGenerator" => "classes/general/FishGenerator/Generators/PhotoGenerator.php",
@@ -17,7 +18,6 @@ include_once 'classes/general/Libs/faker/src/autoload.php';
     "\GGrach\FishGenerator\Generators\ElementGenerator" => "classes/general/FishGenerator/Generators/ElementGenerator.php",
     "\GGrach\FishGenerator\Cache\RuntimeCache" => "classes/general/FishGenerator/Cache/RuntimeCache.php",
     "\GGrach\FishGenerator\Parser\RuleGenerationParser" => "classes/general/FishGenerator/Parser/RuleGenerationParser.php",
-    
     // entry point
     "\GGrach\FishGenerator\Generators\FishGenerator" => "classes/general/FishGenerator/Generators/FishGenerator.php",
 ]);
