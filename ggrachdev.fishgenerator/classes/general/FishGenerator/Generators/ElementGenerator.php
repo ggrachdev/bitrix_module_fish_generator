3020ea5cf1d642c556490908423ef1ca96d86e1d
ggrachdev <ggrachdev@yandex.ru>
2021-05-31 10:42:35 +0400

    Fix generator

--- ggrachdev.fishgenerator/classes/general/FishGenerator/Generators/ElementGenerator.php
+++ ggrachdev.fishgenerator/classes/general/FishGenerator/Generators/ElementGenerator.php
@@ -115,100 +115,20 @@ class ElementGenerator extends PropertyRulesElementFilter {
 
         $valuePropety = null;
 
+        if (!empty($arParams[0])) {
+            $params = explode(',', $arParams[0]);
+        }
+        else
+        {
+            $params = [];
+        }
+
         // @todo Вынести в отдельную сущность, написать через рефлексию генерации где нет параметров
         switch ($typeGenerator) {
-            case 'name':
-                $valuePropety = $this->dataGenerator->name;
-                break;
-
-            case 'inn':
-                $valuePropety = $this->dataGenerator->inn;
-                break;
-
-            case 'kpp':
-                $valuePropety = $this->dataGenerator->kpp;
-                break;
-
-            case 'address':
-                $valuePropety = $this->dataGenerator->address;
-                break;
-
-            case 'text':
-                $valuePropety = $this->dataGenerator->text;
-                break;
-
-            case 'word':
-                $valuePropety = $this->dataGenerator->word;
-                break;
-
-            case 'city':
-                $valuePropety = $this->dataGenerator->city;
-                break;
-
-            case 'country':
-                $valuePropety = $this->dataGenerator->country;
-                break;
-
-            case 'phoneNumber':
-                $valuePropety = $this->dataGenerator->phoneNumber;
-                break;
-
-            case 'company':
-                $valuePropety = $this->dataGenerator->company;
-                break;
-
-            case 'email':
-                $valuePropety = $this->dataGenerator->email;
-                break;
-
-            case 'streetAddress':
-                $valuePropety = $this->dataGenerator->streetAddress;
-                break;
-
-            case 'date':
-                // @todo Добавить формат
-                $valuePropety = $this->dataGenerator->date;
-                break;
-
-            case 'time':
-                // @todo Добавить формат
-                $valuePropety = $this->dataGenerator->time;
-                break;
-
-            case 'year':
-                $valuePropety = $this->dataGenerator->year;
-                break;
-
-            case 'jobTitle':
-                $valuePropety = $this->dataGenerator->jobTitle;
-                break;
-
-            case 'lastName':
-                $valuePropety = $this->dataGenerator->lastName;
-                break;
-
-            case 'firstName':
-                $valuePropety = $this->dataGenerator->firstName;
-                break;
-
-            case 'hexcolor':
-                $valuePropety = $this->dataGenerator->hexcolor;
-                break;
-
-            case 'latitude':
-                $valuePropety = $this->dataGenerator->latitude;
-                break;
-
-            case 'longitude':
-                $valuePropety = $this->dataGenerator->longitude;
-                break;
-
             case 'image':
 
                 $height = 1000;
                 $width = 1000;
-
-                $params = explode(',', $arParams[0]);
 
                 if (!empty($params)) {
                     if (is_numeric($params[0])) {
@@ -242,13 +162,6 @@ class ElementGenerator extends PropertyRulesElementFilter {
                     $linkImg = 'https://loremflickr.com/'.$width.'/'.$height.'/'.$categoryPhoto.'?salt='. \uniqid();
                     $valuePropety = $this->generatePhotoFromLink($linkImg);
                 }
-                break;
-
-            case 'numberBetween':
-                // @todo Сделать (1, 100) а не (1)(100)
-                $numberFrom = is_numeric($arParams[0]) ? $arParams[0] : 0;
-                $numberTo = is_numeric($arParams[1]) ? $arParams[1] : 100;
-                $valuePropety = $this->dataGenerator->numberBetween($numberFrom, $numberTo);
                 break;
 
             case 'randomElement':
@@ -288,32 +201,20 @@ class ElementGenerator extends PropertyRulesElementFilter {
                 break;
 
             default:
-                $rc = new \ReflectionClass($this->dataGenerator);       
-                if($rc->hasMethod($typeGenerator))
-                {
-                    try {
-                        if ($count == 1) {
-                            $valuePropety = \call_user_func_array([$this->dataGenerator, $typeGenerator], $arParams);
-                        } else {
-                            $valuePropety = [];
-                            for ($i = 0; $i < $count; $i++) {
-                                $valuePropety[] = \call_user_func_array([$this->dataGenerator, $typeGenerator], $arParams);
-                            }
-                        }
-                    } catch (Exception $ex) {
-                        $this->addError($ex->getMessage());
-
-                        if ($this->isStrictMode) {
-                            throw new GeneratorTypeException($ex->getMessage());
+                try {
+                    if ($count == 1) {
+                        $valuePropety = \call_user_func_array([$this->dataGenerator, $typeGenerator], $params);
+                    } else {
+                        $valuePropety = [];
+                        for ($i = 0; $i < $count; $i++) {
+                            $valuePropety[] = \call_user_func_array([$this->dataGenerator, $typeGenerator], $params);
                         }
                     }
-                }
-                else
-                {
-                    $this->addError('Not found generator type');
+                } catch (Exception $ex) {
+                    $this->addError($ex->getMessage());
 
                     if ($this->isStrictMode) {
-                        throw new GeneratorTypeException('Not found generator type');
+                        throw new GeneratorTypeException($ex->getMessage());
                     }
                 }
                 break;
