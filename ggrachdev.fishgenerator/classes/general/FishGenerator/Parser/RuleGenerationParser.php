72d6652e01371a3c21ee1cd5b9bdbb89a2997b98
ggrachdev <ggrachdev@yandex.ru>
2021-05-31 12:21:40 +0400

    Refactoring

--- ggrachdev.fishgenerator/classes/general/FishGenerator/Parser/RuleGenerationParser.php
+++ ggrachdev.fishgenerator/classes/general/FishGenerator/Parser/RuleGenerationParser.php
@@ -13,6 +13,70 @@ class RuleGenerationParser {
         'NAME', 'ACTIVE', 'CODE', 'IBLOCK_SECTION_ID', 'DETAIL_TEXT', 'PREVIEW_TEXT', 'SORT'
     ];
 
+    private static function isSystemProperty(string $propertyName): bool {
+        $clearPropertyCode = self::getClearPropertyName($propertyName);
+        return in_array($clearPropertyCode, static::SYSTEM_PROPERTIES) && ($propertyName[0] == '*' || $propertyName[1] == '*');
+    }
+    
+    private static function getClearPropertyName($propertyName)
+    {
+        return str_replace(['*', '='], '', $propertyName);
+    }
+
+    private static function generatePropertyValue(string $propertyName, string $typeGenerator, FishGenerator $generator) {
+        $isDefaultValue = false;
+
+        $isSystemBitrixProperty = self::isSystemProperty($propertyName);
+
+        if ($isSystemBitrixProperty) {
+            if ($propertyName[0] === '=' || $propertyName[1] === '=') {
+                $isDefaultValue = true;
+            }
+        } else {
+            if ($propertyName[0] === '=') {
+                $isDefaultValue = true;
+            }
+        }
+
+        if (!is_array($typeGenerator)) {
+            $arParams = self::getParamsFromGeneratorString($typeGenerator);
+            $typeGenerator = array_shift($arParams);
+
+            $valuePropety = null;
+
+            if (!$isDefaultValue) {
+                $valuePropety = $generator->generateItem($typeGenerator, $arParams);
+            } else {
+                $valuePropety = $typeGenerator;
+            }
+        } else {
+
+            if (sizeof($typeGenerator) == 2) {
+
+                if (
+                    is_string($typeGenerator[0]) && 
+                    is_numeric($typeGenerator[1])
+                ) {
+
+                    $count = $typeGenerator[1];
+
+                    $arParams = self::getParamsFromGeneratorString($typeGenerator[0]);
+                    
+                    $typeGenerator = array_shift($arParams);
+
+                    $valuePropety = $generator->generateItem($typeGenerator, $arParams, $count);
+                }
+            }
+        }
+
+        return $valuePropety;
+    }
+    
+    private static function getParamsFromGeneratorString(string $generatorString)
+    {
+        return explode('(', str_replace(')', '', $generatorString));
+    }
+
     public static function parse(array $arInputRules, FishGenerator $generator) {
 
         $arValidPropertiesForGeneration = [
@@ -24,52 +88,16 @@ class RuleGenerationParser {
 
             foreach ($arInputRules as $propertyName => $typeGenerator) {
 
-                $isSystemBitrixProperty = in_array(str_replace(['*', '='], ['', ''], $propertyName), static::SYSTEM_PROPERTIES) && ($propertyName[0] == '*' || $propertyName[1] == '*');
-
-                $isDefaultValue = false;
-
-                if ($isSystemBitrixProperty) {
-                    if ($propertyName[0] === '=' || $propertyName[1] === '=') {
-                        $isDefaultValue = true;
-                    }
-                } else {
-                    if ($propertyName[0] === '=') {
-                        $isDefaultValue = true;
-                    }
-                }
-
-                if (!is_array($typeGenerator)) {
-                    $arParams = explode('(', str_replace(')', '', $typeGenerator));
-                    $typeGenerator = array_shift($arParams);
-
-                    $valuePropety = null;
-
-                    if (!$isDefaultValue) {
-                        $valuePropety = $generator->generateItem($typeGenerator, $arParams);
-                    } else {
-                        $valuePropety = $typeGenerator;
-                    }
-                } else {
-
-                    if (sizeof($typeGenerator) == 2) {
-
-                        if (is_numeric($typeGenerator[1]) && is_string($typeGenerator[0])) {
-
-                            $count = $typeGenerator[1];
-
-                            $arParams = explode('(', str_replace(')', '', $typeGenerator[0]));
-                            $typeGenerator = array_shift($arParams);
-
-                            $valuePropety = $generator->generateItem($typeGenerator, $arParams, $count);
-                        }
-                    }
-                }
+                $valuePropety = self::generatePropertyValue($propertyName, $typeGenerator, $generator);
 
                 if ($valuePropety !== null) {
-                    if ($isSystemBitrixProperty) {
-                        $arValidPropertiesForGeneration['STANDART_PROPERTIES'][str_replace(['*', '='], ['', ''], $propertyName)] = $valuePropety;
+                
+                    $clearPropertyCode = self::getClearPropertyName($propertyName);
+                
+                    if (self::isSystemProperty($propertyName)) {
+                        $arValidPropertiesForGeneration['STANDART_PROPERTIES'][$clearPropertyCode] = $valuePropety;
                     } else {
-                        $arValidPropertiesForGeneration['PROPERTIES'][str_replace(['*', '='], ['', ''], $propertyName)] = $valuePropety;
+                        $arValidPropertiesForGeneration['PROPERTIES'][$clearPropertyCode] = $valuePropety;
                     }
                 }
             }
